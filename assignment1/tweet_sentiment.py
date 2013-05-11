49c7b2eac19efa0f1a96a90d4ed90b78a98df7e2
Amrita Chanda <amrita.chanda@gmail.com>
2013-05-11 17:43:05 +0530

    Completed problem 2 for assignment1. Need to do more testing.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -14,14 +14,29 @@ def hw(tweet_fp):
     scores = makeAFINNdict()
     # print scores.items() # Print every (term, score) pair in the dictionary
     tweettext_list = [] #Creating an array of dicts, each dict is a tweet
+    tweet_sentiment_perline = [] # creating an array of sentiments, one for each tweet
     for line in tweet_fp:
     	tweet = json.loads(line)
+    	this_tweet_sen = 0
     	if tweet.has_key("text"):
     		tweettext_list.append(tweet["text"].encode('utf-8'))
+    		tweettext = tweet["text"].encode('utf-8')
+    		tweettext_words = (tweet["text"].encode('utf-8')).split(" ")
+    		for tweet_word in tweettext_words:
+    			tweet_word = tweet_word.lower()
+    			if scores.has_key(tweet_word):
+    				this_tweet_sen += scores[tweet_word]
+    		print tweettext
+    		print "sentiment: " + str(this_tweet_sen)
+    	else:
+    		print "-------- tweet has no text --------"
 
-    print len(tweettext_list)
+    	tweet_sentiment_perline.append(this_tweet_sen)
+    	
+
+   	# print tweet_sentiment_perline
+    # print len(tweet_sentiment_perline)
     tweet_fp.seek(0) # reset the filepointer to the beginning of the file
-    
 
 
 def lines(fp):
