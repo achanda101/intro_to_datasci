e1aec4d24ebf6942bd14db2e1300888eb82c7764
Amrita Chanda <amrita.chanda@gmail.com>
2013-05-12 20:45:12 +0530

    Completed problem 2. Will submit now.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,53 +1,44 @@
 import sys
 import json
 
-def makeAFINNdict():
-	afinnfile = open("AFINN-111.txt")
+def makeAFINNdict(sentiment_fp):
 	scores = {} # initialize an empty dictionary
-	for line in afinnfile:
+	for line in sentiment_fp:
 		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
 		scores[term] = int(score)  # Convert the score to an integer.
-	afinnfile.close()
 	return scores
 
-def hw(tweet_fp):
-    scores = makeAFINNdict()
-    # print scores.items() # Print every (term, score) pair in the dictionary
-    tweettext_list = [] #Creating an array of dicts, each dict is a tweet
-    tweet_sentiment_perline = [] # creating an array of sentiments, one for each tweet
+def hw(sent_fp, tweet_fp):
+    scores = makeAFINNdict(sent_fp)
     for line in tweet_fp:
     	tweet = json.loads(line)
-    	this_tweet_sen = 0
+    	this_tweet_sent = 0
     	if tweet.has_key("text"):
-    		tweettext_list.append(tweet["text"].encode('utf-8'))
+    		# tweettext_list.append(tweet["text"].encode('utf-8'))
     		tweettext = tweet["text"].encode('utf-8')
     		tweettext_words = (tweet["text"].encode('utf-8')).split(" ")
     		for tweet_word in tweettext_words:
     			tweet_word = tweet_word.lower()
     			if scores.has_key(tweet_word):
-    				this_tweet_sen += scores[tweet_word]
+    				this_tweet_sent += scores[tweet_word]
     		print tweettext
-    		print "sentiment: " + str(this_tweet_sen)
+    		print "sentiment: " + str(this_tweet_sent)
     	else:
-    		print "-------- tweet has no text --------"
-
-    	tweet_sentiment_perline.append(this_tweet_sen)
-    	
-
-   	# print tweet_sentiment_perline
-    # print len(tweet_sentiment_perline)
-    tweet_fp.seek(0) # reset the filepointer to the beginning of the file
-
+    		print "sentiment: 0"
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    # sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[1])
-    hw(tweet_file)
-    # lines(sent_file)
-    lines(tweet_file)
-
+	sent_file  = open(sys.argv[1])
+	tweet_file = open(sys.argv[2])
+	hw(sent_file, tweet_file)
+	sent_file.seek(0) # reset the filepointer to the beginning of the file
+	tweet_file.seek(0)
+	lines(sent_file)
+	lines(tweet_file)
+	sent_file.close()
+	tweet_file.close()
+                                  
 if __name__ == '__main__':
     main()
